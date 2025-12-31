64a34c847b2fa2c054c6210e38fb83069f56c5cf
bullyto <apero.nuit.du.66@gmail.com>
2025-12-31 18:04:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = "gps-v10.9";
+const CACHE = "gps-v11";
 const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png","./data/adresses.csv"];
 self.addEventListener("install",(e)=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate",(e)=>{ e.waitUntil(self.clients.claim()); });
