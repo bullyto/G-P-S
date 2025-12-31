3e6793ed3b39e82f5456f0d4cf04e18447e611a6
bullyto <apero.nuit.du.66@gmail.com>
2025-12-31 04:30:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = "tournee-v8-cache";
+const CACHE = "gpsv8.5";
 const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png","./data/adresses.csv"];
 self.addEventListener("install",(e)=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate",(e)=>{ e.waitUntil(self.clients.claim()); });
